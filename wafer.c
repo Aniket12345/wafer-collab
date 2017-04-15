16f557cf1b6e63abc97f857acfecdae3488afb2e
prasss <prasannamaps@gmail.com>
2017-04-15 12:03:10 +0530

    even odd code is here!

--- wafer.c
+++ wafer.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
+
+int evenodd(int n); //declaration
+
 int main()
 {
-	return 0;
+
+
+return 0;
+}
+
+void evenodd(int n)
+{
+	if(n%2==0)
+{
+	printf("the number is even\n");
+}
+else 
+{
+	printf("the number is odd\n");
+}
+
 }

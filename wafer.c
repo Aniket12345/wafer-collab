820a03c2a95015e510baeecf0444cb8af4a8549b
Aniket1234 <any.fondekar@gmail.com>
2017-04-15 12:28:38 +0530

    wafer even addition

--- wafer.c
+++ wafer.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
+void calling();
 int main()
 {
+
 	return 0;
+}
+void calling()
+{
+	int values,i,sum=0,enter_value;
+	for(i=0;i<10;i++)
+	{
+		scanf("%d",&enter_value);
+		if(enter_value%2==1)
+		{
+			sum=enter_value+sum;
+		}
+
+	}
+	printf("sum of all 0dd no=%d\n",sum);
+
 }

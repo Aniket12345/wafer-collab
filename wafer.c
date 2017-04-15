b81dca35653bd8f9661a9c90b98e8d5338604bad
Daphne Silveira <daphne.miley@gmail.com>
2017-04-15 12:35:56 +0530

    array sum

--- wafer.c
+++ wafer.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
+int add_no();
+
 int main()
 {
 	return 0;
+
+}
+
+int add_no() {
+	int n; 
+	printf("Give me the no. of inputs\n");
+	scanf("%d",&n);
+	int array[n];
+	int sum = 0;
+	printf("give me the no.s\n");
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d",&(array[i]));
+		sum = sum + array[i];
+	}
+
+printf("sum = %d\n",sum );
+
 }

5bb4a2be666c1213141de7e6a72595c2e7fac7bc
gaurav <gauravgadekar2159@gmail.com>
2017-04-15 12:12:21 +0530

    factorial

--- wafer.c
+++ wafer.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+int fact(int);
+
 int main()
-{
+{   
+	
 	return 0;
+}
+
+int fact(int b)
+{
+	int c;
+	int ans=1;
+	for(c=1;c<=b;c++)
+     ans=ans*c;
+ printf("the answer is %d \n",ans);
+ return(ans);
+
 }
